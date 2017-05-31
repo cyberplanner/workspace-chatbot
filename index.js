7952a18688a0d7e706a0a1ddbd474cee462ae9f5
biniam gebreyesus <beniam23@gmail.com>
2017-05-31 11:14:35 +0100

    added index.js script with simple bot that says Hi

---
+++ index.js
@@ -0,0 +1,35 @@
+/*-----------------------------------------------------------------------------
+sample setup to get this started
+
+# RUN THE BOT LOCALLY:
+
+    * Install Bot Framework Emulator
+    * Install ngrok found at https://ngrok.com/.
+    * Run the bot from the command line using "index app.js".
+    * In Emulator enter "http://localhost:3978/api/messages" as endpoint.
+    * say "Hello"
+
+-----------------------------------------------------------------------------*/
+
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("Hi, how can I assist you Today ?");
+});

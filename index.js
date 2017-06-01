a758aab042959d08ac3d6fb46e3b1026bd2e60f4
jigna-shah <jigna.shah21@gmail.com>
2017-06-01 14:19:27 +0100

    added LUIS recogniser and ConsoleConnector

--- index.js
+++ index.js
@@ -13,23 +13,59 @@ sample setup to get this started
 
 var restify = require('restify');
 var builder = require('botbuilder');
-
+var config = require('config');
 // Setup Restify Server
 var server = restify.createServer();
+
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
 
-// Create chat connector for communicating with the Bot Framework Service
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
+process.env.LUIS_APP_ID = 'c61dfa54-fa2f-498b-9264-dd6d749395c3';
+process.env.LUIS_APP_SUBSCRIPTION_KEY = 'f59febdb4cbb48f684f89379958a7272';
 
-// Listen for messages from users 
-server.post('/api/messages', connector.listen());
+// Create chat connector for communicating with the Bot Framework Service, you have to register you Bot to get MICROSOFT_APP_ID and MICROSOFT_APP_PASSWORD
+//var connector = new builder.ChatConnector({
+//    appId: process.env.MICROSOFT_APP_ID,
+//    appPassword: process.env.MICROSOFT_APP_PASSWORD
+//});
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("Hi, how can I assist you Today ?");
-});
+//Create chat bot (to test locally)
+var connector = new builder.ConsoleConnector().listen();
+
+//Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector);
+
+var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/' + process.env.LUIS_APP_ID +'?subscription-key=' + process.env.LUIS_APP_SUBSCRIPTION_KEY;
+var recognizer = new builder.LuisRecognizer(model);
+var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+bot.dialog('/', dialog);
+
+//Get intents configuration
+var intentsConfig = config.get("Bot.intents");
+
+console.error(intentsConfig.holidaysleft.name);
+
+//HolidaysLeft intent. Luis based.
+dialog.matches(intentsConfig.holidaysleft.name, [
+    function (session, args, next) {
+        session.send(intentsConfig.holidaysleft.messages.default);
+    }
+]);
+
+//HolidaysEntitlement intent. Luis based.
+dialog.matches(intentsConfig.holidaysentitlement.name, [
+    function (session, args, next) {
+        session.send(intentsConfig.holidaysentitlement.messages.default);
+    }
+]);
+
+//welcome intent. Luis based.
+dialog.matches(intentsConfig.welcome.name, [
+    function (session, args, next) {
+        session.send(intentsConfig.welcome.messages.default);
+    }
+]);
+
+//default response if users command is unknown.
+dialog.onDefault(builder.DialogAction.send(intentsConfig.none.messages.default));

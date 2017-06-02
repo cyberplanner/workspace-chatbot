de2fc0ab6cbe3a1cedbfefc9fc335ce4426eb8d1
jigna-shah <jigna.shah21@gmail.com>
2017-06-02 08:08:17 +0100

    reading cred through env variables, moved bot components into seperate file

--- index.js
+++ index.js
@@ -10,41 +10,23 @@ sample setup to get this started
     * say "Hello"
 
 -----------------------------------------------------------------------------*/
-
-var restify = require('restify');
-var builder = require('botbuilder');
 var config = require('config');
-// Setup Restify Server
-var server = restify.createServer();
+var botComponents = require('./custom_modules/module_botComponents');
 
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
-});
+var builder = botComponents.getBuilder();
+var bot = botComponents.getBot();
 
-process.env.LUIS_APP_ID = 'c61dfa54-fa2f-498b-9264-dd6d749395c3';
-process.env.LUIS_APP_SUBSCRIPTION_KEY = 'f59febdb4cbb48f684f89379958a7272';
-
-// Create chat connector for communicating with the Bot Framework Service, you have to register you Bot to get MICROSOFT_APP_ID and MICROSOFT_APP_PASSWORD
-//var connector = new builder.ChatConnector({
-//    appId: process.env.MICROSOFT_APP_ID,
-//    appPassword: process.env.MICROSOFT_APP_PASSWORD
-//});
-
-//Create chat bot (to test locally)
-var connector = new builder.ConsoleConnector().listen();
-
-//Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector);
-
-var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/' + process.env.LUIS_APP_ID +'?subscription-key=' + process.env.LUIS_APP_SUBSCRIPTION_KEY;
-var recognizer = new builder.LuisRecognizer(model);
-var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+//=========================================================
+//Bots Dialogs
+//=========================================================
+// Create LUIS recognizer that points at our model and add it as the root '/' dialog for our Cortana Bot.
+var recognizer = botComponents.getRecognizer();
+var dialog = botComponents.getDialog();
 bot.dialog('/', dialog);
 
 //Get intents configuration
 var intentsConfig = config.get("Bot.intents");
 
-console.error(intentsConfig.holidaysleft.name);
 
 //HolidaysLeft intent. Luis based.
 dialog.matches(intentsConfig.holidaysleft.name, [

5c632c6275d47d0382e377c765870d89e967476d
biniam gebreyesus <beniam23@gmail.com>
2017-06-02 09:44:39 +0100

    uncommented lines of code and refactor

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ var builder = require('botbuilder');
 var db = dbcon.getConnection();
 
 // Setup Restify Server
-/*var server = restify.createServer();
+var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
-});*/
+});
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
@@ -29,7 +29,7 @@ var connector = new builder.ChatConnector({
 });
 
 // Listen for messages from users 
-//server.post('/api/messages', connector.listen());
+server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {

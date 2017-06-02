6edcbd02799141f311871f33ed7cbb7c4cc9ac8a
jigna-shah <jigna.shah21@gmail.com>
2017-06-02 23:37:22 +0100

    added test cases for module_botComponents class

--- spec/module_botComponents_spec.js
+++ spec/module_botComponents_spec.js
@@ -1,3 +1,34 @@
-describe("Bot components", function() {
-	console.log("####Test invoked####");
+var botComponents = require("../custom_modules/module_botComponents");
+
+describe("Test botComponents functions", function() {
+	it("Tests that the getBot() is called", function() {
+		spyOn(botComponents, "getBot");
+        botComponents.getBot();
+        expect(botComponents.getBot).toHaveBeenCalled();
+    });
+	
+	it("Tests that the getConnector() is called", function() {
+		spyOn(botComponents, "getConnector");
+        botComponents.getConnector();
+        expect(botComponents.getConnector).toHaveBeenCalled();
+    });
+	
+	it("Tests that the getBuilder() is called", function() {
+		spyOn(botComponents, "getBuilder");
+        botComponents.getBuilder();
+        expect(botComponents.getBuilder).toHaveBeenCalled();
+    });
+	
+	it("Tests that the getRecognizer() is called", function() {
+		spyOn(botComponents, "getRecognizer");
+        botComponents.getRecognizer();
+        expect(botComponents.getRecognizer).toHaveBeenCalled();
+    });
+	
+	it("Tests that the getDialog() is called", function() {
+		spyOn(botComponents, "getDialog");
+        botComponents.getDialog();
+        expect(botComponents.getDialog).toHaveBeenCalled();
+    });
+
 });

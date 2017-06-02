5800db96f943199dc6e933af63c0fc850b705633
biniam gebreyesus <beniam23@gmail.com>
2017-06-02 09:29:09 +0100

    added cloudant db connection

--- index.js
+++ index.js
@@ -10,15 +10,17 @@ sample setup to get this started
     * say "Hello"
 
 -----------------------------------------------------------------------------*/
-
+var dbcon = require('./custom_modules/module_dbConnection');
 var restify = require('restify');
 var builder = require('botbuilder');
 
+var db = dbcon.getConnection();
+
 // Setup Restify Server
-var server = restify.createServer();
+/*var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
-});
+});*/
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
@@ -27,7 +29,7 @@ var connector = new builder.ChatConnector({
 });
 
 // Listen for messages from users 
-server.post('/api/messages', connector.listen());
+//server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {

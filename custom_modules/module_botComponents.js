0e8846c1674af21caad98dd684baa49b01753104
Dan Cotton <dan@daniel-cotton.co.uk>
2017-06-05 10:02:31 +0100

    ES6 upgrade in index + fixed server creation

--- custom_modules/module_botComponents.js
+++ custom_modules/module_botComponents.js
@@ -1,28 +1,17 @@
 var builder = require('botbuilder');
-var restify = require('restify');
-
-
 
 //=========================================================
 // Bot Setup
 //=========================================================
 
-// Setup Restify Server
- var server = restify.createServer();
- server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
- });
-
-
  // Create chat bot (Uncomment before pushing)
 
-  var connector = new builder.ChatConnector({
-  	appId: process.env.MICROSOFT_APP_ID,
-      appPassword: process.env.MICROSOFT_APP_PASSWORD
-  });
+var connector = new builder.ChatConnector({
+	appId: process.env.MICROSOFT_APP_ID,
+		appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
 
-  var bot = new builder.UniversalBot(connector);
-  server.post('/api/messages', connector.listen());
+var bot = new builder.UniversalBot(connector);
 
 //Create chat bot (Uncomment this to test locally)
 

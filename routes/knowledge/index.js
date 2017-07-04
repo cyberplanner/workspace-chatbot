b8d53c39f3c55af7cc381fa84240b3761642c77d
edcallow96 <edcallow@hotmail.co.uk>
2017-07-04 15:59:16 +0100

    Expose endpoint with no given Knowledge Id so Cloudant randomly generates it.

--- routes/knowledge/index.js
+++ routes/knowledge/index.js
@@ -1,6 +1,6 @@
 // Module Imports
 const RestifyRouter = require('restify-routing');
-const validator = require( 'restify-json-schema-validation-middleware' )();
+const validator = require('restify-json-schema-validation-middleware')();
 
 let db;
 
@@ -30,22 +30,52 @@ const createKnowledgeSchema = require('./schemas/createKnowledge.json');
 *       404:
 *        description: doc not found
 */
-knowledgeRouter.post('/:id', 
-	    validator.body( createKnowledgeSchema ),
-	    (req, res) => {
-	    	res.header("Access-Control-Allow-Origin", req.header.origins);
-	        db.insert(Object.assign(req.body, {
-	            _id: req.params.id
-	        }))
-	        .then(() => {
-	            res.json(200, {message: "Successfully saved knowledge."});
-	        })
-	        .catch(error => {
-	            console.log(error);  
-	            res.json(500, {error: error.reason});
-	        });
-	    });
+knowledgeRouter.post('/:id',
+	validator.body(createKnowledgeSchema),
+	(req, res) => {
+		res.header("Access-Control-Allow-Origin", req.header.origins);
+		db.insert(Object.assign(req.body, {
+			_id: req.params.id
+		}))
+			.then(() => {
+				res.json(200, { message: "Successfully saved knowledge." });
+			})
+			.catch(error => {
+				console.log(error);
+				res.json(500, { error: error.reason });
+			});
+	});
 
+
+/**
+* @swagger
+* /knowledge/:
+*   post:
+*     description: Creates a new Knowledge Management item in storage.
+*     properties:
+*       id:
+*         type: string  
+*     responses:
+*       200:
+*         description: Successful Creation
+*       500:
+*         description: Creation failed. Item may already exist in DB.
+*       404:
+*        description: doc not found
+*/
+knowledgeRouter.post('/',
+	validator.body(createKnowledgeSchema),
+	(req, res) => {
+		res.header("Access-Control-Allow-Origin", req.header.origins);
+		var knowledge = db.insert(Object.assign(req.body))
+			.then(() => {
+				res.json(200, { message: "Successfully saved knowledge with ID " + db._id });
+			})
+			.catch(error => {
+				console.log(error);
+				res.json(500, { error: error.reason });
+			});
+	});
 
 
 /**
@@ -66,17 +96,17 @@ knowledgeRouter.post('/:id',
  *       404:
  *          description: doc not found
  */
-knowledgeRouter.get('/:id', 
-    (req,res) => {
-        db.get(req.params.id)
-        .then(doc => {
-            res.json(doc);
-        })
-        .catch(err => {
-            console.log("Error :"+err)
-            res.json(err.statusCode, {error: err.reason});
-        });
-    });
+knowledgeRouter.get('/:id',
+	(req, res) => {
+		db.get(req.params.id)
+			.then(doc => {
+				res.json(doc);
+			})
+			.catch(err => {
+				console.log("Error :" + err)
+				res.json(err.statusCode, { error: err.reason });
+			});
+	});
 
 
 /**
@@ -92,17 +122,17 @@ knowledgeRouter.get('/:id',
  *       404:
  *          description: doc not found
  */
-knowledgeRouter.get('/', 
-    (req,res) => {
-    		db.list({include_docs:true})
-	        .then(doc => {
-	            res.json(doc);
-	        })
-	        .catch(err => {
-	            console.log("Error :"+err)
-	            res.json(err.statusCode, {error: err.reason});
-	        });
-    });
+knowledgeRouter.get('/',
+	(req, res) => {
+		db.list({ include_docs: true })
+			.then(doc => {
+				res.json(doc);
+			})
+			.catch(err => {
+				console.log("Error :" + err)
+				res.json(err.statusCode, { error: err.reason });
+			});
+	});
 
 
 /**
@@ -121,31 +151,31 @@ knowledgeRouter.get('/',
 *       500:
 *         description: Update failed. Item may not exist in DB.
 */
- knowledgeRouter.put('/:id', 
- validator.body( createKnowledgeSchema ),
-    (req, res) => {
-    	db.get(req.params.id)
-        .then(doc => {
-              db.insert(Object.assign(req.body, {
-  		            _rev: doc._rev,
-  		            _id:  req.params.id
-  		      }))
-  		      .then(() => {
-  		           res.json(200, {message: "Successfully updated knowledge."});
-  		      })
-  		      .catch(error => {
-  		            console.error(error);  
-  		            res.json(500, {error: error.reason});
-  		      });
-        })
-        .catch(err => {
-            console.log("Error :"+err)
-            res.json(err.statusCode, {error: err.reason});
-        });		      
-    });
+knowledgeRouter.put('/:id',
+	validator.body(createKnowledgeSchema),
+	(req, res) => {
+		db.get(req.params.id)
+			.then(doc => {
+				db.insert(Object.assign(req.body, {
+					_rev: doc._rev,
+					_id: req.params.id
+				}))
+					.then(() => {
+						res.json(200, { message: "Successfully updated knowledge." });
+					})
+					.catch(error => {
+						console.error(error);
+						res.json(500, { error: error.reason });
+					});
+			})
+			.catch(err => {
+				console.log("Error :" + err)
+				res.json(err.statusCode, { error: err.reason });
+			});
+	});
 
 
 module.exports = (database) => {
-    db = database;
-    return knowledgeRouter
+	db = database;
+	return knowledgeRouter
 };

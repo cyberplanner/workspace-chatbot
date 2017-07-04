98f8eed1a8df8583217d8527687d1a51297e6872
edcallow96 <edcallow@hotmail.co.uk>
2017-07-04 16:53:07 +0100

    Fix indentation.

--- routes/knowledge/index.js
+++ routes/knowledge/index.js
@@ -79,6 +79,37 @@ knowledgeRouter.post('/',
 
 
 /**
+* @swagger
+* /knowledge/:
+*   post:
+*     description: Creates a new Knowledge Management item in storage.
+*     properties:
+*       id:
+*         type: string  
+*     responses:
+*       200:
+*         description: Successful Creation
+*       500:
+*         description: Creation failed. Item may already exist in DB.
+*       404:
+*        description: doc not found
+*/
+knowledgeRouter.post('/',
+  validator.body(createKnowledgeSchema),
+  (req, res) => {
+    res.header("Access-Control-Allow-Origin", req.header.origins);
+    var knowledge = db.insert(Object.assign(req.body))
+      .then(() => {
+        res.json(200, { message: "Successfully saved knowledge.", id: db._id });
+      })
+      .catch(error => {
+        console.log(error);
+        res.json(500, { error: error.reason });
+      });
+  });
+
+
+/**
  * @swagger
  * /knowledge/:id:
  *   get:

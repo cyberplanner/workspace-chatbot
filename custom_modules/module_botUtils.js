f8069eb0b6ae3cd04a34bfc922b8c3354045a9a6
Dan Cotton <dan@daniel-cotton.co.uk>
2017-07-06 10:29:19 +0100

    moved check conditions to utils

--- custom_modules/module_botUtils.js
+++ custom_modules/module_botUtils.js
@@ -37,6 +37,49 @@ const processResponse = (session, message) => {
   return newMessage.trim();
 }
 
+const checkConditions = (node, session, args, next) => {
+  if (node.conditions && node.conditions.length > 0) {
+    return node.conditions.reduce((condition, result) => {
+      if (result) {
+
+        let entity = args.entities.find(entity => {
+          return entity.type === condition.entityId;
+        });
+
+        let result = entity.resolution.values.reduce((value, result) => {
+          if (result) {
+            return true;
+          } else {
+            switch (condition.comparator) {
+              case "EQUALS":
+                return value === condition.value;
+              case "CONTAINS":
+                return value.contains(condition.value);
+              case "REGEX_MATCH":
+                return value.match(new RegExp(condition.value));
+              default:
+                return false;
+            }
+          }
+        }, false);
+
+        if (condition.not) {
+          return !result;
+        } else {
+          return result;
+        }
+
+      } else {
+        // Give up - a condition hasn't been met
+        return false;
+      }
+    }, true);
+  } else {
+    // No conditions present - just return true.
+    return true;
+  }
+}
 module.exports = {
-  processResponse: processResponse
+  processResponse: processResponse,
+  checkConditions: checkConditions
 }

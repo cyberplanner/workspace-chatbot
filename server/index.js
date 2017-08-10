8014b498771d76726f8d438937b356ca274c3c31
Dan Cotton <dan@daniel-cotton.co.uk>
2017-08-10 13:16:30 +0100

    Added allowed origins fix

--- server/index.js
+++ server/index.js
@@ -150,6 +150,20 @@ const root = `${__dirname}/public`;
 //=========================================================
 
 server.use(bodyParser.json());
+server.use((req, res, next) => {
+    if (process.env.CROSS_SITE_ORIGINS) {
+        let allowedOrigins = process.env.CROSS_SITE_ORIGINS.split(',');
+   
+        let origin = req.headers.origin;
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        if(allowedOrigins.indexOf(origin) > -1){
+        res.setHeader('Access-Control-Allow-Origin', origin);
+        }
+        return next();
+    }
+});
 /*server.use(restify.CORS({
   origins: process.env.CROSS_SITE_ORIGINS.split(","),
   credentials: false

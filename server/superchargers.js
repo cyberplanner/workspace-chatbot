ef82f0d6b019709369a11b5b4427604af14e4d50
Dan Cotton <dan.cotton@capgemini.com>
2017-10-13 15:28:40 +0100

    Added bulk supercharger registration, reworked process of registering superchargers to be simpler

--- server/superchargers.js
+++ server/superchargers.js
@@ -1,24 +1,9 @@
-const genericSuperchargers = require("./superchargers/genericSuperchargers");
-
 /*
   Supercharger registration document
 */
-const init = () => {
-  let supercharger = require("./lib/supercharger");
-  supercharger
-    .clear()
-    .then(() => {
-      /**
-       * Register all Generic Superchargers.
-       */
-      supercharger.register(genericSuperchargers.optionalQuestion);
-      supercharger.register(genericSuperchargers.storeAnswer);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+const supercharger = require("./lib/supercharger");
+const genericSuperchargers = require("./superchargers/genericSuperchargers");
 
-  return supercharger;
-};
-
-module.exports = init;
+// Register all Generic Superchargers.
+supercharger.register(genericSuperchargers.optionalQuestion);
+supercharger.register(genericSuperchargers.storeAnswer);

2105072436d3386425b5be67ba6b2b8b124681fe
Dan Cotton <dan.cotton@capgemini.com>
2017-10-20 19:36:37 +0100

    Added JSDoc comment to executor

--- server/lib/middleware/executor.js
+++ server/lib/middleware/executor.js
@@ -1,5 +1,13 @@
 const logger = require("../../logger");
 
+/**
+ * Code to iterate through and execute middleware sequentially, 
+ * assuming that it supports the action type provided.
+ * @param {Middleware} botMiddleware An array of Middleware objects
+ * @param {*} action One of the exported, enumerated TYPES
+ * @param {*} event The botbuilder middleware event.
+ * @param {*} next The "next" function provided by botbuilder.
+ */
 const execute = (botMiddleware, action, event, next) => {
   logger.info(`[MIDDLEWARE-EXECUTOR] Executing events of type: ${action}`);
   let current = -1;
